1dbd5e6ef976748d5d999bcc53f5d1abc3a6217a
Manu Manoj <manumanoj4540@gmail.com>
2020-06-27 17:02:41 +0530

    Added Styling and hero elements

--- src/components/header.js
+++ src/components/header.js
@@ -12,27 +12,40 @@ class Head extends Component {
  render(){
      return(
         <>
-            <Navbar dark color='dark' expand="md">
-                <div className='container'>
-                <NavbarBrand className="mr-auto" href="/">Manu Manoj</NavbarBrand>
-                <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar className="try ml-auto">
-                                <NavItem>
-                                    <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-sm"></span> Home</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to='/aboutus'> About Us</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link"  to='/menu'> Menu</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to='/contactus'> Contact Us</NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
+           
+            <div className="hero">
+                <Navbar dark expand="md">
+                    <div className='container'>
+                    <NavbarBrand className="mr-auto" href="/">Manu Manoj</NavbarBrand>
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
+                                <Nav navbar className=" ml-auto">
+                                    <NavItem>
+                                        <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-sm"></span> Home</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to='/aboutus'> About Us</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className="nav-link"  to='/menu'> Menu</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to='/contactus'> Contact Us</NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </Collapse>
+                    </div>
+                </Navbar>
+                <div className= "container">
+                    <div className= "row row-header">
+                        <div className="col-12 col-sm-6 ">
+                            <h1>Ristorante con Fusion</h1>
+                            <p> We take inspiration from the World's best cuisines, and create a unique fusion experience.
+                                Our lipsmacking creations will tickle your culinary senses!
+                            </p>
+                        </div>
+                    </div>
                 </div>
-            </Navbar>
+            </div>
         </>
      )
  }
