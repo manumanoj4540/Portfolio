12968a8beecc5f531e69cedded4ebc8f92688cb5
Manu Manoj <manumanoj4540@gmail.com>
2020-06-28 11:18:25 +0530

    Added intro animation using spring-react

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,9 @@
 import React , { Component } from 'react'
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Modal, Button, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
+import {Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Modal, Button, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Card,CardBody, CardHeader } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import logo from '../img/logo.png'
+import dp from '../img/dp.jpg'
+import { Spring } from 'react-spring/renderprops'
 
 class Head extends Component {
  constructor (props) {
@@ -16,7 +19,7 @@ class Head extends Component {
             <div className="hero">
                 <Navbar dark expand="md">
                     <div className='container'>
-                    <NavbarBrand className="mr-auto" href="/">Manu Manoj</NavbarBrand>
+                    <NavbarBrand className="mr-auto" href="/"><img src={logo} height="50" width="150" alt='Ristorante Con Fusion' /></NavbarBrand>
                     <Collapse isOpen={this.state.isNavOpen} navbar>
                                 <Nav navbar className=" ml-auto">
                                     <NavItem>
@@ -37,11 +40,29 @@ class Head extends Component {
                 </Navbar>
                 <div className= "container">
                     <div className= "row row-header">
-                        <div className="col-12 col-sm-6 ">
-                            <h1>Ristorante con Fusion</h1>
-                            <p> We take inspiration from the World's best cuisines, and create a unique fusion experience.
-                                Our lipsmacking creations will tickle your culinary senses!
-                            </p>
+                        <div className="col-12 col-sm-6 offset-sm-6">                           
+                            <Spring 
+                            from={{opacity:0}}
+                            to={{opacity:1}}
+                            config={{duration:1500, delay:900}}>
+                            {
+                                props =>(
+                                <div style={props}>
+                                    <div className="row"> 
+                                        <img src={dp} alt="Manu Manoj" width="175" height="160" className="col-12 col-md-4 test"/>                                                      
+                                        <div className="col-12 col-md intro">
+                                            <h3>A Quick Intro</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            </p>
+                                        </div>             
+                                    </div>
+                                </div>
+                                    )
+                            }
+                            </Spring>                            
                         </div>
                     </div>
                 </div>
@@ -51,3 +72,6 @@ class Head extends Component {
  }
 }
 export default Head ;
+                                 
+                                    
+                                       
--- src/components/main.js
+++ src/components/main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Head from './header'
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 
+
 class Main extends Component {
     constructor(props){
         super(props);
