752b929fa6fd13439a0caea5a5af31ce68f8e784
Manu Manoj <manumanoj4540@gmail.com>
2020-07-08 18:40:02 +0530

    Replaced class comp with hooks

--- src/components/header.js
+++ src/components/header.js
@@ -1,54 +1,41 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import {Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import logo from '../img/logo.png'
 
-export class Header extends Component {
-    constructor (props) {
-        super (props) ;
-        this.toggleNav = this.toggleNav.bind(this);
-        this.state = {
-        isNavOpen: false
-        };
-    }
-
-    toggleNav() {
-    this.setState({ isNavOpen: !this.state.isNavOpen });
-    }
-
-    
-    render() {
-        return (
-            <div className="navi">
-                <Navbar dark expand="md">
-                    <div className='container'>
-                        <div className="row">
-                        <NavbarToggler onClick={this.toggleNav}  className="col-3"/>
-                        <NavbarBrand className="mr-auto col" href="/home"><img src={logo} height="50" width="150" alt='Manu Manoj' /></NavbarBrand>
-                        </div>
-                        
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                                    <Nav navbar className=" ml-auto">
-                                        <NavItem>
-                                            <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-sm"></span> Home</NavLink>
-                                        </NavItem>
-                                        <NavItem>
-                                            <a className="nav-link" href="#About"> About Me</a>
-                                        </NavItem>
-                                        <NavItem>
-                                            <NavLink className="nav-link"  to='/project'> Projects</NavLink>
-                                        </NavItem>
-                                        <NavItem>
-                                            <NavLink className="nav-link" to='/contact'> Contact Me</NavLink>
-                                        </NavItem>
-                                    </Nav>
-                        </Collapse>
+const Header = () => {
+    const [isNavOpen,toggleNav] = useState(false);    
+    return (
+        <div className="navi">
+            <Navbar dark expand="md">
+                <div className='container'>
+                    <div className="row">
+                    <NavbarToggler onClick={() => toggleNav(!isNavOpen)}  className="col-3"/>
+                    <NavbarBrand className="mr-auto col" href="/home"><img src={logo} height="50" width="150" alt='Manu Manoj' /></NavbarBrand>
                     </div>
-                </Navbar>
-                
-            </div>
-        )
-    }
+                    
+                    <Collapse isOpen={isNavOpen} navbar>
+                            <Nav navbar className=" ml-auto">
+                                <NavItem>
+                                    <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-sm"></span> Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <a className="nav-link" href="#About"> About Me</a>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link"  to='/project'> Projects</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/contact'> Contact Me</NavLink>
+                                </NavItem>
+                            </Nav>
+                    </Collapse>
+                </div>
+            </Navbar>
+            
+        </div>
+    )
+    
 }
 
 export default Header
--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,6 @@ import { Spring } from 'react-spring/renderprops'
 import '../App.css'
 
 function Home () {
-
     function description() {
         return(
             <Spring 
@@ -17,15 +16,15 @@ function Home () {
                 {
                     props =>(
                         <div style={props}>                                                    
-                        <div>
-                            <h3 className="col-12">A Quick Intro</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
-                            <Link to='/about'><button className="btn offset-md-6"><p>Contact Me</p></button></Link>
-                        </div>                                  
+                            <div>
+                                <h3 className="col-12">A Quick Intro</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </p>
+                                <Link to='/about'><button className="btn offset-md-6"><p>Contact Me</p></button></Link>
+                            </div>                                  
                         </div>
                     )
                 }
